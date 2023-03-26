8ad70a86dab59fbcbe3cc9601f9be892300fdf71
Hugo <hugo.saraiva.nascimento@gmail.com>
2023-03-26 16:20:33 -0300

    Adding codegen and mutations on backend

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -1,6 +1,9 @@
 import { createYoga, createSchema } from "graphql-yoga"
 import type { NextApiRequest, NextApiResponse } from "next"
 import psql from "serverless-postgres"
+import sql, { Sql, empty, join } from "sql-template-tag"
+import { Resolvers, Task, TaskStatus } from "../../../generated/graphql-backend"
+import { GraphQLError } from "graphql"
 
 interface ServerContext {
   req: NextApiRequest
@@ -8,16 +11,6 @@ interface ServerContext {
 }
 
 type Context = Awaited<ReturnType<typeof context>>
-
-type Resolvers<TContext = any> = {
-  [parentKey: string]: {
-    [fieldKey: string]: (
-      parent: unknown,
-      args: unknown,
-      context: TContext
-    ) => any
-  }
-}
 
 const typeDefs = `
   enum TaskStatus {
@@ -57,30 +50,103 @@ const typeDefs = `
   }
 `
 
+interface TaskDbRow {
+  id: number
+  title: string
+  task_status: TaskStatus
+}
+
+const getFistElementSafe = <T>(input: T[]) => (input.length ? input[0] : null)
+
+class TaskSelector {
+  constructor(private db: Context["db"]) {}
+  getById: (id: TaskDbRow["id"]) => Promise<Task | null> = async (id) => {
+    const tasks = await getDbQueryResult<TaskDbRow[]>(
+      this.db,
+      sql`SELECT id, title, task_status FROM tasks WHERE id = ${id};`
+    )
+    const task = getFistElementSafe(tasks)
+    return task
+      ? { id: task.id, title: task.title, status: task.task_status }
+      : null
+  }
+}
+
+async function getDbQueryResult<TResult>(
+  db: Context["db"],
+  query: Sql
+): Promise<TResult> {
+  await db.connect()
+  const dbQueryResult = await db.query(query)
+  await db.clean()
+  const result = dbQueryResult.rows
+  return result
+}
+
 const resolvers: Resolvers<Context> = {
   Query: {
-    async tasks(parent, args, { db }) {
-      await db.connect()
-      const dbQueryResult = (await db.query(
-        "SELECT 'HELLO WORLD' AS hello_world;"
-      )) as { rows: { hello_word: string }[] }
-      const result = dbQueryResult.rows[0]
-      await db.clean()
-      return []
+    async tasks(_, { status }, { db }) {
+      const tasks = await getDbQueryResult<TaskDbRow[]>(
+        db,
+        sql`SELECT id, title, task_status FROM tasks ${
+          status ? sql`WHERE task_status = ${status}` : empty
+        };`
+      )
+      return tasks.map((task) => ({
+        id: task.id,
+        title: task.title,
+        status: task.task_status,
+      }))
     },
-    task(parent, args, context) {
-      return null
+    async task(_, { id }, { db }) {
+      const selector = new TaskSelector(db)
+      return selector.getById(Number(id))
     },
   },
   Mutation: {
-    createTask(parent, args, context) {
-      return null
+    async createTask(_, { input: { title } }, { db }) {
+      const taskId = await getDbQueryResult<{ id: number }[]>(
+        db,
+        sql`INSERT INTO tasks (title, task_status) VALUES (${title}, ${TaskStatus.Active}) RETURNING id;`
+      ).then((idsObj) => getFistElementSafe(idsObj)?.id)
+      if (!taskId) {
+        return null
+      }
+      return { id: taskId, title: title, status: TaskStatus.Active }
     },
-    updateTask(parent, args, context) {
-      return null
+    async updateTask(_, { input: { id, title, status } }, { db }) {
+      if (!title && !status) {
+        throw new GraphQLError("At least one argument to update must be sent")
+      }
+      const selector = new TaskSelector(db)
+      const task = await selector.getById(Number(id))
+      if (!task) {
+        throw new GraphQLError("Could not find task")
+      }
+      const result = await getDbQueryResult<[]>(
+        db,
+        sql`UPDATE tasks SET ${join(
+          [
+            title ? sql`title=${title}` : empty,
+            title ? ` , ` : empty,
+            status ? sql`task_status=${status}` : empty,
+          ],
+          " "
+        )} WHERE id=${id};`
+      )
+      return selector.getById(Number(id))
     },
-    deleteTask(parent, args, context) {
-      return null
+    async deleteTask(_, { input: { id } }, { db }) {
+      const selector = new TaskSelector(db)
+      const taskToBeDeleted = await selector.getById(Number(id))
+      if (!taskToBeDeleted) {
+        throw new GraphQLError("Could not find task")
+      }
+      const result = await getDbQueryResult<[]>(
+        db,
+        sql`DELETE FROM tasks WHERE id=${id};`
+      )
+      return taskToBeDeleted
     },
   },
 }
